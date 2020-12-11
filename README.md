64394c9a56ae437696b519f0ffbc36123b4fcfed
Rohit Gupta <rohit.gupta@wingify.com>
2020-12-11 17:23:05 +0530

    added comments and updated readme

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ git clone https://github.com/rohitgupta13991/returnFlights.git
 2. Now execute: webdriver-manager update (use sudo if permission access errors)
 3. Now execute: webdriver-manager start (use sudo if permission access errors)
 4. Open another terminal window, switch to this repo and run the command: protractor conf.js
-
-It will launch chrome browser and will execute test cases.
-
+5. It will launch chrome browser, execute test cases and PRINT 20 CHEAPEST COMBOS IN CONSOLE.
+6. You can change destination and departure cities in texts.json. Format will be 3 letter code of city.
 
--- utilities/commons.js
+++ utilities/commons.js
@@ -7,11 +7,13 @@ var depPrices = {};
 var retPrices = {};
 exports.getSearchFlight = function () {
     'use strict';
+    //Setting dep date as 30 days from current date and ret date as 40 days from current date
     var depDate_ts = date.setDate(date.getDate() + 30);
     var returnDate_ts = date.setDate(date.getDate() + 40);
     var depDate = timestampToDDMMYYYY(depDate_ts);
     var returnDate = timestampToDDMMYYYY(returnDate_ts);
 
+    //Filling details on ixigo.com and clicking search button
     searchFlight.flight.departureCity.sendKeys(text.departureCity);
     browser.wait(EC.textToBePresentInElement(searchFlight.flight.departureCityResult,text.departureCity), 10000);
     searchFlight.flight.departureCityResult.click();
@@ -32,6 +34,8 @@ exports.getSearchFlight = function () {
     browser.wait(EC.visibilityOf(searchFlight.flight.searchResult.get(0)), 60000);
     expect(searchFlight.flight.searchResult.length).not.toBeLessThan(20);
 };
+
+//Function to convert timestamp to DDMMYYYY format
 var timestampToDDMMYYYY = function (ts) {
     var date_ob = new Date(ts);
     var date_year = date_ob.getFullYear();
@@ -40,6 +44,8 @@ var timestampToDDMMYYYY = function (ts) {
     var date = date_days+date_month+date_year;
     return date;
 };
+
+//Function to fetch dep and ret prices in 2 dictionaries
 exports.fetchPrices = async function () {
     'use strict';
     //wait for all the results to load
@@ -75,26 +81,22 @@ exports.fetchPrices = async function () {
 
 exports.cheapestCombo = async function () {
 
-    //sorting both the price dictionary
+    //sorting both the price dictionaries and storing results in 2 different 2D arrays
     var array1 = await Object.keys(depPrices).map(function(key) {
         return [key, depPrices[key]];
     });
-
     await array1.sort(function(first, second) {
         return first[1] - second[1];
     });
-    //console.log("SORTED DEP ARRAY "+array1[0][0]+array1[0][1]);
-
     var array2 = await Object.keys(retPrices).map(function(key) {
         return [key, retPrices[key]];
     });
-
     await array2.sort(function(first, second) {
         return first[1] - second[1];
     });
-    //console.log("SORTED RET ARRAY "+array2[0][0]+array2[0][1]);
 
 
+    //Logic to calculate top 20 cheapest combos from 2 sorted 2D arrays.
     var index = new Array(20).fill(0);
     var noOfComb = 20;
 
@@ -128,7 +130,7 @@ exports.cheapestCombo = async function () {
     });
 
         console.log("(" + dText + " ₹" +
-            array1[min_index][1] + ")          (" + rText + "  ₹" +
+            array1[min_index][1] + ")           (" + rText + "  ₹" +
             array2[index[min_index]][1] + ") ");
 
         index[min_index]++;
